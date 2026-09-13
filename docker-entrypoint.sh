cc03f16aa7bbde812d4958f0a9ba2ddb822e2340
Lautaro Matas <lmatas@gmail.com>
2026-09-13 11:32:47 +0200

    Configure explorer public base path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-sed -i "s|https://mainnet.infura.io/alethio|/jsonrpc|g" /usr/share/nginx/html/js/app.*.js
+# The distribution was built with an absolute SPA base.  Make it relocatable
+# at container startup so a declarative edge route can publish it at
+# /explorer/ without leaking assets or Vue history links to the gateway root.
+sed -i "s|https://mainnet.infura.io/alethio|/jsonrpc|g; s|c.p=\"/\"|c.p=\"/explorer/\"|g; s|mode:\"history\",routes:|base:\"/explorer/\",mode:\"history\",routes:|g; s|BASE_URL:\"/\"|BASE_URL:\"/explorer/\"|g" /usr/share/nginx/html/js/app.*.js
+sed -i 's|href=/|href=/explorer/|g; s|src=/|src=/explorer/|g' /usr/share/nginx/html/index.html
 exec /docker-entrypoint.sh "$@"
